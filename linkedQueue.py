71c9892d4c58f2990960881c943be819e48afe6a
josenelson2 <jose3202nelson@gmail.com>
2025-04-08 14:44:06 -0300

    Final do trabalho de ED

--- linkedQueue.py
+++ linkedQueue.py
@@ -10,14 +10,46 @@ class Queue:
         new_node = Node(name) #novo nó para guardar o nome
         if self.head is None: #verificar se a lista está vazia
             self.head = new_node #a cabeça da fila passa a ser o nome
-            self.tail = new_node #o final tbm,já que temos apenas um nome
+            self.tail = new_node #o final tbm, já que temos apenas um nome
         else:
             self.tail.next = new_node #o último nó passa a apontar para o novo nó
             self.tail = new_node #o novo nó é o último da fila
         self.size += 1 #aumenta a contagem de pessoas na fila
-        return "Nome foi enfileirado"
+        return name
     
-    def dequeue(self):
-        pass
+    def dequeue(self): #método para excluir um nome da fila
+        if self.head is None: #confere se a fila está vazia
+            return None
+        removed_name = self.head.name #guarda o primeiro nome da fila
+        self.head = self.head.next #o primeiro da fila passa a ser o seguinte
+        self.size -= 1 #decrementa uma posição da fila
+        if self.head is None: #confere se a fila ficou vazia após a remoção
+            self.tail = None #o final da fila passa a ser none
+        return removed_name
+    
+    def find(self, name): #método para encontrar nome
+        current = self.head #começa a procurar do primeiro nó da fila
+        while current: #enquanto o nó for verdade
+            if current.name == name:
+                return name #retorna nome se for acahado
+            current = current.next #nó é substituído pelo próximo
+        return None #retorna None se não achar nada
+            
+
+    def showNames(self):
+        names = []
+        current = self.head
+
+        while current is not None:
+            names.append(current.name)
+            current = current.next
+        return names
+        
+
+    def showSize(self):
+        return self.size
+
+
+
     
         
--- main.py
+++ main.py
@@ -1,2 +1,56 @@
 from linkedQueue import Queue
 
+def menu():
+    print("\n==== Menu Queue Ativ 4 ====")
+    print("1. Adicionar (enqueue)")
+    print("2. Remover (dequeue)")
+    print("3. Mostrar elementos da fila")
+    print("4. Mostrar tamanho da fila")
+    print("5. Procurar nome na fila")
+    print("0. Sair")
+
+fila = Queue()
+
+while True:
+    menu()
+    op = int(input("Escolha uma opção: "))
+
+    while op < 0 or op > 5:
+        op = int(input("Opção inválida! Digite novamente: ")) 
+     
+    if op == 1:
+        name = input("Digite o nome que deseja adicionar: ")
+        fila.enqueue(name)
+        print(f"Pronto. O elemento '{name}' foi adicionado!")
+
+    elif op == 2:
+        name = fila.dequeue()
+        if name == None:
+            print("Fila vazia!")
+        else:
+            print(f"Pronto. O elemento '{name}' foi removido!")
+
+    elif op == 3:
+        nomes = fila.showNames()
+        if nomes:
+            print("Nomes das pessoas na fila")
+            print(" -> ".join(nomes))
+        else:
+            print("Nao ha pessoas na fila.")
+
+    elif op == 4:
+        quant = fila.showSize()
+        print(f"A quantidade de pessoas na fila eh {quant}.")
+
+    elif op == 5:
+        name = input("Qual nome deseja encontrar: ")
+        resul = fila.find(name)
+        if resul == None:
+            print("Nome não encontrado!")
+        else:
+            print("Nome encontrado!")
+            
+    else:
+        break
+
+
